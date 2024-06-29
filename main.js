920b76c2f329611469296d021b6e329bd4f33fd8
QuratFaheem <mrs.faheem16@gmail.com>
2024-06-29 12:47:55 +0500

    done

---
+++ main.js
@@ -0,0 +1,93 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let enemies = ["Skeleton", "Zombie", "Warrior", "Assassin"];
+let maxEnemyHealth = 75;
+let enemyAttackDamage = 25;
+let health = 100;
+let attackDamage = 50;
+let numHealthPotions = 3;
+let healthPotionHealAmount = 30;
+let healthPotionDropChance = 50; // 50% chance
+let running = true;
+console.log("WELCOME TO MY ADVENTURE GAME");
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+function gameLoop() {
+    console.log("----------------------------------------------------------");
+    let enemyHealth = getRandomInt(maxEnemyHealth) + 1;
+    let enemyIndex = getRandomInt(enemies.length);
+    let enemy = enemies[enemyIndex];
+    console.log(`\t${enemy} has appeared!\n`);
+    const combat = () => {
+        if (enemyHealth > 0 && health > 0) {
+            console.log(`\tYour HP: ${health}`);
+            console.log(`\t${enemy}'s HP: ${enemyHealth}`);
+            console.log(`\n\tWhat would you like to do?`);
+            console.log(`\t1. Attack`);
+            console.log(`\t2. Drink Health Potion`);
+            console.log(`\t3. Run!`);
+            readline.question('> ', (playerAction) => {
+                switch (playerAction) {
+                    case '1':
+                        // Attack the enemy
+                        let damageDealt = getRandomInt(attackDamage) + 1;
+                        let damageTaken = getRandomInt(enemyAttackDamage) + 1;
+                        enemyHealth -= damageDealt;
+                        health -= damageTaken;
+                        console.log(`\t> You strike the ${enemy} for ${damageDealt} damage.`);
+                        console.log(`\t> You receive ${damageTaken} damage.`);
+                        if (health <= 0) {
+                            console.log(`\t> You have taken too much damage, you are too weak to go on!`);
+                            running = false;
+                            readline.close();
+                            return;
+                        }
+                        if (enemyHealth <= 0) {
+                            console.log(`\t> ${enemy} was defeated!`);
+                            let dropChance = getRandomInt(100);
+                            if (dropChance < healthPotionDropChance) {
+                                numHealthPotions++;
+                                console.log(`\t> The ${enemy} dropped a health potion!`);
+                                console.log(`\t> You now have ${numHealthPotions} health potion(s).`);
+                            }
+                            gameLoop();
+                        }
+                        else {
+                            combat();
+                        }
+                        break;
+                    case '2':
+                        // Drink health potion
+                        if (numHealthPotions > 0) {
+                            health += healthPotionHealAmount;
+                            numHealthPotions--;
+                            console.log(`\t> You drink a health potion, healing for ${healthPotionHealAmount}.`);
+                            console.log(`\t> You now have ${health} HP.`);
+                            console.log(`\t> You have ${numHealthPotions} health potion(s) left.`);
+                        }
+                        else {
+                            console.log(`\t> You have no health potions left! Defeat enemies for a chance to get one!`);
+                        }
+                        combat();
+                        break;
+                    case '3':
+                        // Run away
+                        console.log(`\tYou run away from the ${enemy}!`);
+                        gameLoop();
+                        break;
+                    default:
+                        console.log(`\tInvalid command!`);
+                        combat();
+                        break;
+                }
+            });
+        }
+    };
+    combat();
+}
+gameLoop();
